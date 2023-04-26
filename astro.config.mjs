e0106ae8a8913d1e5227b2befde1618dabfa6293
duexcoast <conor.ux@gmail.com>
2023-04-26 01:31:42 -0400

    basic components complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   server: {
     port: 3111,
-    host: true,
+    host: true
   },
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
