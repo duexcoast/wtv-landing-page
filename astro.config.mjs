c988742517660bf6bc3a6af9c59be73565f2c815
duexcoast <conor.ux@gmail.com>
2023-04-25 21:11:07 -0400

    Use WheelTV logo for favicon.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://astroship.web3templates.com",
+  server: {
+    port: 3111,
+    host: true,
+  },
   integrations: [
     tailwind(),
     image({
